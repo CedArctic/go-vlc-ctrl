fad73f0dde45b3a2e2f1a9db2ff02bbf0853271b
CedArctic <kdoumani98@gmail.com>
2020-04-03 18:03:18 +0300

    vlm.go: - Removed HTTP status code returning from the signatures since HTTP status code errors are now checked by RequestMaker()

--- vlm.go
+++ vlm.go
@@ -5,19 +5,19 @@ import (
 )
 
 // Get the full list of VLM elements
-func (instance *VLC) Vlm() (response string, statusCode int, err error) {
-	response, _, statusCode, err = instance.RequestMaker("/requests/vlm.xml")
+func (instance *VLC) Vlm() (response string, err error) {
+	response, _, _, err = instance.RequestMaker("/requests/vlm.xml")
 	return
 }
 
 // Execute VLM Command. Command is internally URL percent-encoded
-func (instance *VLC) VlmCmd(cmd string) (response string, statusCode int, err error) {
-	response, _, statusCode, err = instance.RequestMaker("/requests/vlm_cmd.xml?command=" + url.QueryEscape(cmd))
+func (instance *VLC) VlmCmd(cmd string) (response string, err error) {
+	response, _, _, err = instance.RequestMaker("/requests/vlm_cmd.xml?command=" + url.QueryEscape(cmd))
 	return
 }
 
 // Get last VLM Error
-func (instance *VLC) VlmCmdErr() (response string, statusCode int, err error) {
-	response, _, statusCode, err = instance.RequestMaker("/requests/vlm_cmd.xml")
+func (instance *VLC) VlmCmdErr() (response string, err error) {
+	response, _, _, err = instance.RequestMaker("/requests/vlm_cmd.xml")
 	return
 }
